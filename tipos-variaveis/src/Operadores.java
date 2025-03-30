08202e92e6f8d422f0da289df442468406135192
EvertonSantos3 <ton.everton.santos@gmail.com>
2025-03-30 13:30:32 -0300

    Arquivos novos

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,21 @@
+public class Operadores {
+    public static void main(String[] args) {
+
+       boolean condição1 = true;
+       boolean condição2 = false;
+
+       if (condição1 && ( 7 > 4 )) {
+        System.out.println("As condicões são verdadeiras");
+
+        }
+        
+        if (condição1 || condição2) {
+
+            System.out.println("Uma das consições é verdadeira");
+
+        }
+
+        System.out.println("Fim !!!");
+            
+    }
+}
